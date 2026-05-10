597b9f6fb2c308e084b579449b967b97daa5c97f
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-05-10 10:26:52 +0200

    add section how-to-play

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './js/burgerMenu';
+
+import Swiper from 'swiper';
+import 'swiper/css';
+
+new Swiper('.how-swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  grabCursor: true,
+});
