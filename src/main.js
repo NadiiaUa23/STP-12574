23ef08b96cf3d8f723daff5d531a2d840b6030f2
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-05-10 14:57:55 +0200

    move swiper JS

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
 import './js/burgerMenu';
-
-import Swiper from 'swiper';
-import 'swiper/css';
-
-new Swiper('.how-swiper', {
-  slidesPerView: 'auto',
-  spaceBetween: 16,
-  grabCursor: true,
-});
+import './js/swiper';
